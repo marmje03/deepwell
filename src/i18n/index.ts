f7d95d7e1c3ad6abd8ce830363f3a1cd27665bb7
Martha Mjelde <hei@redenstudio.no>
2026-07-05 11:12:08 +0200

    Set up React Router and i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,13 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './locales/en/translation.json'
+import no from './locales/no/translation.json'
+
+i18n.use(initReactI18next).init({
+  resources: { en: { translation: en }, no: { translation: no } },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+})
+
+export default i18n
